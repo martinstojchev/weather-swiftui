df929e415fde555ac2b1e9545157a457441d9b8b
Martin <martinstojchev@hotmail.com>
2023-02-05 14:22:27 +0100

    Initial Commit

---
+++ weather-swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  weather-swiftui
+//
+//  Created by Martin Stojcev on 2023-02-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather-swiftui/weather_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_swiftuiApp.swift
+//  weather-swiftui
+//
+//  Created by Martin Stojcev on 2023-02-05.
+//
+
+import SwiftUI
+
+@main
+struct weather_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
